e3ec653b742e833c1dc87e7177396ba0e8683990
Данилыч <116429660+Gagachka@users.noreply.github.com>
2022-10-22 12:09:07 +0300

    пупупупупупуп
    помогите мне с отладкой

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("Введите ваше имя: ");
+            name = Console.ReadLine();
+            Console.WriteLine("привет,как твои дела? ",name, "!");
+        }
+    }
+}
